4a2a69cdf3feb331194eb9c935ad651346f56989
JibrilCSX <joshi.goischke@gmail.com>
2021-04-16 21:13:43 +0200

    Fixed timezone and DST

--- matchscheduleparser/matchscheduleparser.py
+++ matchscheduleparser/matchscheduleparser.py
@@ -1,9 +1,9 @@
 import discord
 import json
 import requests
-from datetime import datetime, timedelta
 from redbot.core import commands
 from redbot.core.utils.chat_formatting import text_to_file
+from river_mwclient.wiki_time_parser import time_from_str
 
 # links
 SCHEDULE = "https://esports-api.lolesports.com/persisted/gw/getSchedule?hl=en-US&leagueId={}"
@@ -14,7 +14,7 @@ LEAGUES = "https://esports-api.lolesports.com/persisted/gw/getLeagues?hl=en-US"
 START = """== {0} ==
 {{{{SetPatch|patch= |disabled= |hotfix= |footnote=}}}}
 {{{{MatchSchedule/Start|tab={0} |bestof={1} |shownname= }}}}\n"""
-MATCH = """{{{{MatchSchedule|<!-- Do not change the order of team1 and team2!! -->|initialorder={initialorder}|team1={t1} |team2={t2} |team1score= |team2score= |winner=
+MATCH = """{{{{MatchSchedule|<!-- Do not change the order of team1 and team2!! -->|team1={t1} |team2={t2} |team1score= |team2score= |winner=
 |date={date} |time={time} |timezone={timezone} |dst={dst} |pbp= |color= |vodinterview= |with= |stream={stream} |reddit=\n{games}\n}}}}\n"""
 BO1_GAMES = """|game1={{MatchSchedule/Game\n|blue= |red= |winner= |ssel= |ff=\n|mh=\n|recap=\n|vodpb=\n|vodstart=\n|vodpost=\n|vodhl=\n|vodinterview=\n|with=\n|mvp=\n}}"""
 BO2_GAMES = """|game1={{MatchSchedule/Game\n|blue= |red= |winner= |ssel= |ff=\n|mh=\n|recap=\n|vodpb=\n|vodstart=\n|vodpost=\n|vodhl=\n|vodinterview=\n|with=\n|mvp=\n}}
@@ -28,6 +28,9 @@ BO5_GAMES = """|game1={{MatchSchedule/Game\n|blue= |red= |winner= |ssel= |ff=\n|
 |game4={{MatchSchedule/Game\n|blue= |red= |winner= |ssel= |ff=\n|mh=\n|recap=\n|vodpb=\n|vodstart=\n|vodpost=\n|vodhl=\n|vodinterview=\n|with=\n|mvp=\n}}
 |game5={{MatchSchedule/Game\n|blue= |red= |winner= |ssel= |ff=\n|mh=\n|recap=\n|vodpb=\n|vodstart=\n|vodpost=\n|vodhl=\n|vodinterview=\n|with=\n|mvp=\n}}"""
 END = "{{MatchSchedule/End}}\n"
+
+
+ERROR_MESSAGE = "An error has occured. {} might not exist. If your input contains spaces, try again using quotes!"
 
 
 class MatchScheduleParser(commands.Cog):
@@ -44,11 +47,11 @@ class MatchScheduleParser(commands.Cog):
             try:
                 schedule = get_schedule(tournament.upper(), stream)
             except TypeError:
-                await ctx.send(
-                    "An error has occured. {} might not exist. If your input has spaces, try again using quotes.".format(tournament))
+                await ctx.send(ERROR_MESSAGE.format(tournament))
                 return
         await ctx.author.send(file=text_to_file(schedule, filename="matchschedule.txt"))
-        await ctx.send("Check your DMs!")
+        if not isinstance(ctx.channel, discord.channel.DMChannel):
+            await ctx.send("Check your DMs!")
 
     @lolesportsparser.command()
     async def list(self, ctx):
@@ -110,19 +113,12 @@ def filter_json(json_file, *args):
     return new_json
 
 
-def parse_schedule(jsons, timezone="CET", dst="no", stream=""):
-    initialorder = 1
+def parse_schedule(jsons, stream=""):
     schedule, current_tab = "", ""
     for json_file in jsons:
         json_schedule = filter_json(json_file, "data", "schedule", "events")
         for game in json_schedule:
-            date_time = game["startTime"]
-            date = date_time[:10]
-            time = date_time[11:16]
-            time_obj = datetime.strptime(time, "%H:%M")
-            hour = timedelta(hours=1)
-            time_obj = time_obj + hour
-            time = time_obj.strftime("%H:%M")
+            parsed_time = time_from_str(game["startTime"], tz="UTC")
             display = game["blockName"]
             team1 = game["match"]["teams"][0]["name"]
             team2 = game["match"]["teams"][1]["name"]
@@ -130,24 +126,22 @@ def parse_schedule(jsons, timezone="CET", dst="no", stream=""):
             if display != current_tab:
                 schedule = schedule + END + START.format(display, bestof)
                 current_tab = display
-                initialorder = 1
             if bestof == 1:
-                schedule = schedule + MATCH.format(initialorder=initialorder, t1=team1, t2=team2, date=date, time=time,
-                                                   timezone=timezone, dst=dst, stream=stream, games=BO1_GAMES)
+                schedule = schedule + MATCH.format(t1=team1, t2=team2, date=parsed_time.cet_date, time=parsed_time.cet_time,
+                                                   timezone="CET", dst=parsed_time.dst, stream=stream, games=BO1_GAMES)
             elif bestof == 2:
-                schedule = schedule + MATCH.format(initialorder=initialorder, t1=team1, t2=team2, date=date, time=time,
-                                                   timezone=timezone, dst=dst, stream=stream, games=BO2_GAMES)
+                schedule = schedule + MATCH.format(t1=team1, t2=team2, date=parsed_time.cet_date, time=parsed_time.cet_time,
+                                                   timezone="CET", dst=parsed_time.dst, stream=stream, games=BO2_GAMES)
             elif bestof == 3:
-                schedule = schedule + MATCH.format(initialorder=initialorder, t1=team1, t2=team2, date=date, time=time,
-                                                   timezone=timezone, dst=dst, stream=stream, games=BO3_GAMES)
+                schedule = schedule + MATCH.format(t1=team1, t2=team2, date=parsed_time.cet_date, time=parsed_time.cet_time,
+                                                   timezone="CET", dst=parsed_time.dst, stream=stream, games=BO3_GAMES)
             elif bestof == 5:
-                schedule = schedule + MATCH.format(initialorder=initialorder, t1=team1, t2=team2, date=date, time=time,
-                                                   timezone=timezone, dst=dst, stream=stream, games=BO5_GAMES)
+                schedule = schedule + MATCH.format(t1=team1, t2=team2, date=parsed_time.cet_date, time=parsed_time.cet_time,
+                                                   timezone="CET", dst=parsed_time.dst, stream=stream, games=BO5_GAMES)
             else:
                 # Todo: Throw an exception or something
-                schedule = schedule + MATCH.format(initialorder=initialorder, t1=team1, t2=team2, date=date, time=time,
-                                                   timezone=timezone, dst=dst, stream=stream, games=BO1_GAMES)
-            initialorder += 1
+                schedule = schedule + MATCH.format(t1=team1, t2=team2, date=parsed_time.cet_date, time=parsed_time.cet_time,
+                                                   timezone="CET", dst=parsed_time.dst, stream=stream, games=BO1_GAMES)
     schedule = schedule.replace("{{MatchSchedule/End}}\n", "", 1)
     schedule = schedule + END
     return schedule
