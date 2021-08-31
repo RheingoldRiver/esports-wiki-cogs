15ac03deba99d937689085164c5f7bd3e6a8210c
AA <chasehult@gmail.com>
2021-08-31 08:54:53 -0700

    Updates BayesGAMH

--- bayesgamh/bayes_api_wrapper.py
+++ bayesgamh/bayes_api_wrapper.py
@@ -11,7 +11,8 @@ from bayesgamh.errors import BadRequestException
 
 logger = logging.getLogger('red.esports-wiki-cogs.bayesgahm')
 
-GameID = AssetType = Tag = str
+GameID = AssetType = str
+Tag = Union[str, Literal['NULL', 'ALL']]
 
 
 class Game(TypedDict):
@@ -37,44 +38,34 @@ class BayesAPIWrapper:
         self.session = session
 
         self.access_token = None
-        # self.refresh_token = None
         self.expires = datetime.min
 
-    async def ensure_login(self) -> None:
+    async def _ensure_login(self, force_relogin: bool = False) -> None:
         """Ensure that the access_token is recent and valid"""
         keys = await self.bot.get_shared_api_tokens("bayes")
         if not ("username" in keys and "password" in keys):
             raise NoAPIKeyException((await self.bot.get_valid_prefixes())[0]
                                     + f"set api bayes username <USERNAME> password <PASSWORD>")
-        if self.access_token is None or self.expires < datetime.now():
+        if self.access_token is None:
             try:
-                data = await self.do_api_call('POST', 'login',
-                                              {'username': keys['username'], 'password': keys['password']})
+                data = await self._do_api_call('POST', 'login',
+                                               {'username': keys['username'], 'password': keys['password']})
             except ClientResponseError as e:
                 if e.status == 500:
                     raise BadAPIKeyException((await self.bot.get_valid_prefixes())[0]
                                              + f"set api bayes username <USERNAME> password <PASSWORD>")
                 raise
             self.access_token = data['accessToken']
-            # self.refresh_token = data['refreshToken']
             self.expires = datetime.now() + timedelta(seconds=data['expiresIn'] - 30)  # 30 second buffer to be safe
-        # # While the documentation mentions a refresh token, it's not actually supplied on an actual login
-        # # TODO: Maybe follow up w/ Bayes to see what's up with that ^^?
-        # elif self.expires < datetime.now():
-        #     data = await self.do_api_call('POST', 'login/refresh_token', {'refreshToken': self.refresh_token})
-        #     if not data['success']:
-        #         raise RuntimeError(f'Failed to refresh login: {data}')
-        #     self.access_token = data['accessToken']
-        #     self.expires = datetime.now() + timedelta(seconds=data['expiresIn'] - 30)  # 30 second buffer to be safe
 
-    async def do_api_call(self, method: Literal['GET', 'POST'], service: str, data: Dict[str, Any] = None):
+    async def _do_api_call(self, method: Literal['GET', 'POST'], service: str, data: Dict[str, Any] = None):
         """Make a single API call to emh-api.bayesesports.com"""
         endpoint = "https://emh-api.bayesesports.com/"
         if data is None:
             data = {}
 
         if method == "GET":
-            async with self.session.get(endpoint + service, headers=await self.get_headers(), params=data) as resp:
+            async with self.session.get(endpoint + service, headers=await self._get_headers(), params=data) as resp:
                 resp.raise_for_status()
                 data = await resp.json()
         elif method == "POST":
@@ -85,38 +76,57 @@ class BayesAPIWrapper:
             raise ValueError("HTTP Method must be GET or POST.")
         return data
 
-    async def get_headers(self) -> Dict[str, str]:
+    async def _get_headers(self) -> Dict[str, str]:
         """Return headers for a GET request to the API"""
-        await self.ensure_login()
+        await self._ensure_login()
         return {'Authorization': f'Bearer {self.access_token}'}
+
+    def _clean_game(self, game: Game) -> Game:
+        """Add the NULL tag to a game with no tags."""
+        if not game['tags']:
+            game['tags'].append('NULL')
+        return game
 
     async def get_tags(self) -> List[Tag]:
         """Return a list of tags that can be used to request games"""
-        return await self.do_api_call('GET', 'api/v1/tags')
+        return ['NULL'] + await self._do_api_call('GET', 'api/v1/tags')
 
-    async def get_games(self, *, tags: Iterable[Tag], page: int = 0, page_size: int = 20,
-                        from_timestamp: Union[datetime, str] = datetime.min,
-                        to_timestamp: Union[datetime, str] = datetime.max) \
+    async def get_games(self, *, page: Optional[int] = None, page_size: Optional[int] = None,
+                        from_timestamp: Optional[Union[datetime, str]] = None,
+                        to_timestamp: Optional[Union[datetime, str]] = None,
+                        tags: Optional[Iterable[Tag]] = None) \
             -> GetGamesResponse:
         """Make an API query to the api/v1/games endpoint"""
         if isinstance(from_timestamp, datetime):
             from_timestamp = from_timestamp.isoformat()
         if isinstance(to_timestamp, datetime):
             to_timestamp = to_timestamp.isoformat()
-        tags = ','.join(tags)
+        tags = ','.join(tags) if tags is not None else None
         params = {'page': page, 'size': page_size, 'from_timestamp': from_timestamp,
                   'to_timestamp': to_timestamp, 'tags': tags}
-        return await self.do_api_call('GET', 'api/v1/games', params)
+        params = {k: v for k, v in params.items() if v is not None}
+        return await self._do_api_call('GET', 'api/v1/games', params)
 
-    async def get_all_games(self, *, tag: Optional[Tag] = None, tags: Optional[List[Tag]] = None,
-                            from_timestamp: Union[datetime, str] = datetime.min,
-                            to_timestamp: Union[datetime, str] = datetime.max) \
+    async def get_all_games(self, *, tag: Optional[Tag] = None, tags: Optional[Iterable[Tag]] = None,
+                            from_timestamp: Optional[Union[datetime, str]] = None,
+                            to_timestamp: Optional[Union[datetime, str]] = None) \
             -> List[Game]:
-        """Get all games with a tag within the timestamps"""
+        """Get all games with the given filters"""
         if tags is None:
             tags = []
+        else:
+            tags = list(tags)
+        only_null = False
         if tag is not None:
             tags.append(tag)
+        if not tags or tags == ['ALL']:
+            tags = None
+        elif tags == ["NULL"]:
+            only_null = True
+            tags = None
+        elif "NULL" in tags or "ALL" in tags:
+            raise ValueError("The special tags NULL and ALL must be requested alone.")
+
         data = await self.get_games(tags=tags, page_size=999,
                                     from_timestamp=from_timestamp,
                                     to_timestamp=to_timestamp)
@@ -130,24 +140,24 @@ class BayesAPIWrapper:
                 if not newpage['games']:
                     break
                 page += 1
-        return data['games']
+        return [self._clean_game(game) for game in data['games'] if not (game['tags'] and only_null)]
 
     async def get_game(self, game_id: GameID) -> Game:
         """Get a game by its ID"""
         try:
-            game = await self.do_api_call('GET', f'api/v1/games/{game_id}')
+            game = await self._do_api_call('GET', f'api/v1/games/{game_id}')
         except ClientResponseError as e:
             if e.status == 404:
                 raise BadRequestException(f'Invalid Game ID: {game_id}')
             raise
-        return game
+        return self._clean_game(game)
 
     async def get_asset(self, game_id: GameID, asset: AssetType) -> bytes:
         """Get the bytes for an asset"""
         game = await self.get_game(game_id)
         if asset not in game['assets']:
             raise BadRequestException(f'Invalid asset type for game with ID {game_id}: {asset}')
-        data = await self.do_api_call('GET', f'api/v1/games/{game_id}/download', {'type': asset})
+        data = await self._do_api_call('GET', f'api/v1/games/{game_id}/download', {'type': asset})
         fp = BytesIO()
         async with self.session.get(data['url']) as resp:
             return await resp.read()
