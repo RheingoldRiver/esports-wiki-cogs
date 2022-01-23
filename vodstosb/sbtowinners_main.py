16fa18bdeb5a153a6911cb94119023c072873a14
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2022-01-22 21:55:49 -0600

    Refactor sbtowinners

--- vodstosb/sbtowinners_main.py
+++ vodstosb/sbtowinners_main.py
@@ -2,67 +2,87 @@ from mwrogue.esports_client import EsportsClient
 from mwrogue.auth_credentials import AuthCredentials
 import mwparserfromhell
 
-credentials = AuthCredentials(user_file="bot")
-site = EsportsClient("lol", credentials=credentials)
 
-response = site.cargo_client.query(
-    tables="ScoreboardGames=SG, MatchScheduleGame=MSG",
-    fields="SG.Team1,SG.Team2,SG.WinTeam,MSG.N_MatchInTab,MSG.N_TabInPage,MSG.N_GameInMatch,MSG._pageName=Page",
-    join_on="MSG.GameId=SG.GameId",
-    where=f"(MSG.Blue IS NULL OR MSG.Red IS NULL OR MSG.Winner IS NULL) AND (SG.Team1 IS NOT NULL OR SG.Team2 IS NOT NULL OR SG.WinTeam IS NOT NULL)",
-)
+class SbToWinners:
+    summary = "Discover sides & winners from the SB & populate in the row"
+    
+    def __init__(self, site: EsportsClient):
+        self.site = site
+    
+    def run(self):
+        fields = [
+            "SG.Team1",
+            "SG.Team2",
+            "SG.WinTeam",
+            "SG.MatchHistory",
+            "MSG.N_MatchInTab",
+            "MSG.N_TabInPage",
+            "MSG.N_GameInMatch",
+            "MSG._pageName=DataPage",
+        ]
+        result = self.site.cargo_client.query(
+            tables="ScoreboardGames=SG, MatchScheduleGame=MSG",
+            fields=fields,
+            join_on="MSG.GameId=SG.GameId",
+            where=f"(MSG.Blue IS NULL OR MSG.Red IS NULL OR MSG.Winner IS NULL) "
+                  f"AND (SG.Team1 IS NOT NULL OR SG.Team2 IS NOT NULL OR SG.WinTeam IS NOT NULL)",
+            order_by='MSG._pageName'
+        )
+        
+        current_page = {
+            'page': None,
+            'wikitext': None,
+            'page_name': None,
+            'old_text': None,
+        }
+        
+        for item in result:
+            tab_target = int(item['N TabInPage'])
+            match_target = int(item['N MatchInTab'])
+            game_target = int(item['N GameInMatch'])
+            
+            if current_page['page_name'] != item['DataPage']:
+                if current_page['page'] is not None:
+                    self.save_page(current_page)
+                current_page['page_name'] = item['DataPage']
+                current_page['page'] = self.site.client.pages[current_page['page_name']]
+                old_text = current_page['page'].text()
+                current_page['old_text'] = old_text
+                current_page['wikitext'] = mwparserfromhell.parse(old_text)
+            
+            tab_counter = 0
+            match_counter = 0
+            game_counter = 0
+            for template in current_page['wikitext'].filter_templates():
+                if template.name.matches("MatchSchedule/Start"):
+                    tab_counter += 1
+                    match_counter = 0
+                elif template.name.matches("MatchSchedule"):
+                    match_counter += 1
+                    game_counter = 0
+                elif template.name.matches("MatchSchedule/Game"):
+                    game_counter += 1
+                    if (tab_counter, match_counter, game_counter) == (tab_target, match_target, game_target):
+                        if not template.has("blue", ignore_empty=True):
+                            template.add("blue", item['Team1'])
+                        if not template.has("red", ignore_empty=True):
+                            template.add("red", item['Team2'])
+                        if not template.has("winner", ignore_empty=True):
+                            template.add("winner", item['WinTeam'])
+                        if not template.has("mh"):
+                            template.add('mh', item['MatchHistory'])
+        
+        # we need to catch the last iteration too (assuming we actually did anything)
+        if current_page['page'] is not None:
+            self.save_page(current_page)
+    
+    def save_page(self, page_dict):
+        new_text = str(page_dict['wikitext'])
+        if new_text != page_dict['old_text']:
+            self.site.save(page_dict['page'], new_text, summary=self.summary)
 
-datapages = []
 
-for item in response:
-    datapage = item["Page"]
-    if datapage not in datapages:
-        datapages.append(datapage)
-
-for datapage in datapages:
-    page = site.client.pages[datapage]
-    page_text = page.text()
-    page_wikitext = mwparserfromhell.parse(page_text)
-    items = {}
-    for item in response:
-        if item["Page"] != datapage:
-            continue
-        match_in_tab = str(item["N MatchInTab"])
-        tab_in_page = str(item["N TabInPage"])
-        game_in_match = str(item["N GameInMatch"])
-        blue = item["Team1"].strip()
-        red = item["Team2"].strip()
-        if item["WinTeam"] == item["Team1"]:
-            winner = "1"
-        elif item["WinTeam"] == item["Team2"]:
-            winner = "2"
-        else:
-            winner = ""
-        items[f"{tab_in_page}_{match_in_tab}_{game_in_match}"] = [blue, red, winner]
-    tab_counters = 0
-    match_counters = 0
-    game_counters = 0
-    for template in page_wikitext.filter_templates():
-        if template.name.matches("MatchSchedule/Start"):
-            tab_counters += 1
-            match_counters = 0
-        elif template.name.matches("MatchSchedule"):
-            match_counters += 1
-            game_counters = 0
-        elif template.name.matches("MatchSchedule/Game"):
-            game_counters += 1
-            if f"{tab_counters}_{match_counters}_{game_counters}" in items:
-                blue, red, winner = items.get(f"{tab_counters}_{match_counters}_{game_counters}")
-                if not template.has("blue", ignore_empty=True):
-                    if blue != "":
-                        template.add("blue", blue)
-                if not template.has("red", ignore_empty=True):
-                    if red != "":
-                        template.add("red", red)
-                if not template.has("winner", ignore_empty=True):
-                    if winner != "":
-                        template.add("winner", winner)
-        else:
-            continue
-    if str(page_wikitext) != page_text:
-        page.edit(str(page_wikitext), summary="Automatically adding Winners from Scoreboards")
+if __name__ == '__main__':
+    credentials = AuthCredentials(user_file='me')
+    lol_site = EsportsClient('lol', credentials=credentials)  # Set wiki
+    SbToWinners(lol_site).run()
