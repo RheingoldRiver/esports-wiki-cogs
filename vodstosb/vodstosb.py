19c8d6ef1714f5902381819f8a0fe531e81a66c9
AA <chasehult@gmail.com>
2022-02-28 18:09:04 -0700

    Make bot go DND while vodstosb is running (#52)

--- vodstosb/vodstosb.py
+++ vodstosb/vodstosb.py
@@ -1,6 +1,7 @@
 import rivercogutils as utils
 from redbot.core import commands
 from requests.exceptions import ReadTimeout
+from tsutils.user_interaction import StatusManager
 
 from vodstosb.vodstosb_main import VodsToSbRunner
 
@@ -8,7 +9,9 @@ from vodstosb.vodstosb_main import VodsToSbRunner
 class VodsToSb(commands.Cog):
     """Discovers & updates scoreboards on Leaguepedia that are missing vods"""
     
-    def __init__(self, bot):
+    def __init__(self, bot, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
         self.bot = bot
         self.vod_params = ['VodPB', 'VodGameStart', 'Vod', 'VodPostgame']
         self.pages_to_save = {}
@@ -19,7 +22,8 @@ class VodsToSb(commands.Cog):
         
         await ctx.send('Okay, starting now!')
         try:
-            VodsToSbRunner(site, self.vod_params).run()
+            async with StatusManager(self.bot):
+                VodsToSbRunner(site, self.vod_params).run()
         except ReadTimeout:
             await ctx.send('Whoops, the site is taking too long to respond, try again later')
             return
