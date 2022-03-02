443aaf86e4e1be241b3410e0bbea648b1a403380
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2022-03-02 17:55:49 -0600

    mhtowinners - _do_the_thing refactor

--- mhtowinners/mhtowinners.py
+++ mhtowinners/mhtowinners.py
@@ -17,39 +17,25 @@ class MhToWinners(commands.Cog):
     
     @commands.command(pass_context=True)
     async def mhtowinners(self, ctx):
-        await ctx.send('Okay, starting now!')
-        credentials = await utils.get_credentials(ctx, self.bot)
-        site = EsportsClient('lol', credentials=credentials, max_retries=1)
-        try:
-            async with StatusManager(self.bot):
-                MhToWinnersRunner(site).run()
-        except ReadTimeout:
-            return await ctx.send('Whoops, the site is taking too long to respond, try again later')
-        await ctx.send('Okay, done!')
+        self._do_the_thing(ctx, MhToWinnersRunner)
     
     @commands.command(pass_context=True)
     async def sbtowinners(self, ctx):
-        await ctx.send('Okay, starting now!')
-        credentials = await utils.get_credentials(ctx, self.bot)
-        site = EsportsClient('lol', credentials=credentials, max_retries=1)
-        try:
-            async with StatusManager(self.bot):
-                SbToWinnersRunner(site).run()
-        except ReadTimeout:
-            return await ctx.send('Whoops, the site is taking too long to respond, try again later')
-        await ctx.send('Okay, done!')
+        self._do_the_thing(ctx, SbToWinnersRunner)
     
     @commands.command(pass_context=True)
     async def vodstosb(self, ctx):
-        credentials = await utils.get_credentials(ctx, self.bot)
-        site = EsportsClient('lol', credentials=credentials, max_retries=1)
-        
         vod_params = ['VodPB', 'VodGameStart', 'Vod', 'VodPostgame']
-        
+        self._do_the_thing(ctx, VodsToSbRunner, vod_params)
+
+    def _do_the_thing(self, ctx, the_thing, *args):
         await ctx.send('Okay, starting now!')
+        credentials = await utils.get_credentials(ctx, self.bot)
+        site = EsportsClient('lol', credentials=credentials,
+                             max_retries=1, retry_interval=1)
         try:
             async with StatusManager(self.bot):
-                VodsToSbRunner(site, vod_params).run()
+                the_thing(site, *args).run()
         except ReadTimeout:
             return await ctx.send('Whoops, the site is taking too long to respond, try again later')
         await ctx.send('Okay, done!')
