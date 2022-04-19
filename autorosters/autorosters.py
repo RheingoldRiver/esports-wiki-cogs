ca168ca8f739c33a1ae2d5d2821db2f1b89718a6
ArbolitoLoco <36609914+arbolitoloco1@users.noreply.github.com>
2022-04-19 16:55:32 -0300

    Limited command to LoL-Staff

--- autorosters/autorosters.py
+++ autorosters/autorosters.py
@@ -1,9 +1,19 @@
 from mwrogue.esports_client import EsportsClient
 from rivercogutils import utils
-from redbot.core import commands
+from redbot.core import commands, utils as redutils
 from asyncio import TimeoutError
 
 from .autorosters_main import AutoRostersRunner
+
+
+async def is_lol_staff(ctx) -> bool:
+    for role in ctx.message.guild.roles:
+        if role.name == "LoL-Staff":
+            staff_role = role
+            break
+    if staff_role not in ctx.author.roles:
+        raise commands.UserFeedbackCheckFailure("You don't have enough permissions to run this command!")
+    return True
 
 
 class AutoRosters(commands.Cog):
@@ -13,11 +23,12 @@ class AutoRosters(commands.Cog):
         self.bot = bot
 
     @commands.command(pass_context=True)
+    @commands.check(is_lol_staff)
     async def autorosters(self, ctx, *, overview_page):
         """Generate team rosters for the specified tournament"""
         def check(msg):
             return msg.author == ctx.author and msg.channel == ctx.channel
-        await ctx.send("Tabs `Eg. LEC 2022`:")
+        await ctx.send("Tabs: (Eg. `LEC 2022`)")
         try:
             tabs = await self.bot.wait_for("message", check=check, timeout=60)
         except TimeoutError:
